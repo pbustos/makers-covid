3ab4dfd1c5bb8ff73ae9c7504264eac77b365d69
Esteban Martinena Guerrero <orensbruli@gmail.com>
2020-03-23 17:40:02 +0100

    Fixed code of dict diff examples

--- deepdiff_test.py
+++ deepdiff_test.py
@@ -2,22 +2,21 @@ import json
 from copy import deepcopy
 from pprint import pprint
 
-from dictdiffer import diff, patch, swap, revert
+from deepdiff import DeepDiff
 
-
+def reconstruct(d, new):
+    if d.up:
+        print(d.up)
+        print(d)
+        reconstruct(d.up, new)
+    else:
+        return new
 
 with open('data.json') as json_file:
     data = json.load(json_file)
     data2 = deepcopy(data)
-    # Change
     data2["makers"]["@IvanBarbecho"]["Cantidad actual"]="1"
-    # Delete
-    del data2["makers"]["Jesús Merideño (Cacéres y Malpartida CC) @JMergal"]
-    # Add
-    data2["makers"]["orensbruli"] = deepcopy(data2["makers"]["@IvanBarbecho"])
-    data2["makers"]["orensbruli"]["Usuario Telegram"] = "orensbruli"
-
-    # calculate diff
-    result = diff(data, data2, dot_notation=False)
-    result = list(result)
-    pprint(list(result))
+    ddiff = DeepDiff(data, data2, view='tree')
+    for d in ddiff["values_changed"]:
+        reconstruct(d,{})
+    pprint(ddiff)
