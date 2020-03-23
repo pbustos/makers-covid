ac79adbce342da0c35ab31b64d38cf09df09cdc9
Juan Carlos García García <juancarlos97gg@gmail.com>
2020-03-23 18:29:46 +0100

    Cambios en websockets y cliente de prueba

--- client_test.py
+++ client_test.py
@@ -4,8 +4,14 @@ from socketIO_client import SocketIO, LoggingNamespace
 def response(args):
     print('solicitando:', args['data'])
 
+def updated(msg):
+    print('actualización', msg['data'])
+    
 socketIO = SocketIO('localhost', 8000, LoggingNamespace)
 socketIO.on('connection_response', response)
+
+socketIO.on('update', response)
+
 socketIO.emit('connect')
 socketIO.wait(seconds=1)
 
--- websockets.py
+++ websockets.py
@@ -4,7 +4,7 @@ import json
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
-socketio = SocketIO(app)
+socketio = SocketIO(app, cors_allowed_origins="*")
 
 
 @socketio.on('connect')
@@ -17,10 +17,9 @@ def handle_connection():
 
 def update(update):
     print("Broadcasting update")
-    emit('update', { "data" : update}, broadcast=True)
+    socketio.emit('update', { "data" : update}, broadcast=True)
 
 if __name__ == '__main__':
-    socketio.run(app, port=8000)
-
-
+    socketio.run(app, host="127.0.0.1", port=8000)
+    
 
