409e3ea961e80a3fa1e057009d2bfd26c39467f1
Esteban Martinena Guerrero <orensbruli@gmail.com>
2020-03-23 17:37:15 +0100

    deepdiff_test.py updated

--- deepdiff_test.py
+++ deepdiff_test.py
@@ -9,8 +9,15 @@ from dictdiffer import diff, patch, swap, revert
 with open('data.json') as json_file:
     data = json.load(json_file)
     data2 = deepcopy(data)
+    # Change
     data2["makers"]["@IvanBarbecho"]["Cantidad actual"]="1"
-    data2["makers"]["Jesús Merideño (Cacéres y Malpartida CC) @JMergal"]["Cantidad actual"] = "1"
+    # Delete
+    del data2["makers"]["Jesús Merideño (Cacéres y Malpartida CC) @JMergal"]
+    # Add
+    data2["makers"]["orensbruli"] = deepcopy(data2["makers"]["@IvanBarbecho"])
+    data2["makers"]["orensbruli"]["Usuario Telegram"] = "orensbruli"
+
+    # calculate diff
     result = diff(data, data2, dot_notation=False)
     result = list(result)
     pprint(list(result))
