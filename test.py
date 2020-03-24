671be68c38302582b42be2868a07ba5f231fd81b
Esteban Martinena Guerrero <orensbruli@gmail.com>
2020-03-24 09:52:37 +0100

    Added core to reassign types to columns.

--- test.py
+++ test.py
@@ -95,6 +95,11 @@ def clean_and_map_header(header, map):
 
 def remove_special_chars(text):
     return unidecode.unidecode(text)
+
+def reasign_type(types, intput_array):
+    result = [0 if method == 'int' and input == '' else eval(method)(input) for method, input in zip(types, intput_array)]
+    return result
+
 class SpreadsheetCrawler(threading.Thread):
     def __init__(self):
         # Persistence
@@ -130,16 +135,18 @@ class SpreadsheetCrawler(threading.Thread):
         stock_column_range = SHEET_DATA[self.sheet.title]["STOCK_COLUMN_RANGE"]
         stock_initial_column = SHEET_DATA[self.sheet.title]["STOCK_INITIAL_ROW"]
         stock_headers_map = SHEET_DATA[self.sheet.title]["STOCK_HEADERS_MAP"]
-        return self.update_subtable(stock_column_range,stock_initial_column, stock_headers_map)
+        stock_column_types = SHEET_DATA[self.sheet.title]["STOCK_COLUMN_TYPES"]
+        return self.update_subtable(stock_column_range,stock_initial_column, stock_headers_map, stock_column_types)
 
     def update_demand(self):
         demand_column_range = SHEET_DATA[self.sheet.title]["DEMAND_COLUMN_RANGE"]
         demand_initial_column = SHEET_DATA[self.sheet.title]["DEMAND_INITIAL_ROW"]
         demand_headers_map = SHEET_DATA[self.sheet.title]["DEMAND_HEADERS_MAP"]
-        return self.update_subtable(demand_column_range, demand_initial_column, demand_headers_map)
+        demand_column_types = SHEET_DATA[self.sheet.title]["DEMAND_COLUMN_TYPES"]
+        return self.update_subtable(demand_column_range, demand_initial_column, demand_headers_map, demand_column_types)
 
 
-    def update_subtable(self, column_range, initial_row, headers_mapping):
+    def update_subtable(self, column_range, initial_row, headers_mapping, types_map):
         header_range = column_range[0]+str(initial_row)+":"+column_range[-1]+str(initial_row)
         data_range = column_range[0]+str(initial_row+1)+":"+column_range[-1]+str(1000)
         headers = self.sheet.range(header_range)
@@ -149,6 +156,7 @@ class SpreadsheetCrawler(threading.Thread):
         for row in chunks(data,len(char_range(column_range))):
             row_values = list(map(lambda x: remove_special_chars(x.value.strip()), row))
             if row_values[0] != '':
+                row_values = reasign_type(types_map, row_values)
                 row_dict = dict(zip(header_values,row_values))
                 row_dict["Localidad"] = self.sheet.title
                 row_dict["lat"] = ""
